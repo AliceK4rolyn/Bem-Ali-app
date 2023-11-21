0c2ac3368147ceb08f85dbc6982b369f862cbfb3
AliceK4rolyn <codealicekarolyn@gmail.com>
2023-11-21 00:49:04 -0300

    Layout

---
+++ app/src/main/java/com/example/demoapp/Feed_App.kt
@@ -0,0 +1,11 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Feed_App : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed_app)
+    }
+}
