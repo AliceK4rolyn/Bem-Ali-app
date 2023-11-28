0818bd468d8db6431e93243952c8b1c1584ce7b3
AliceK4rolyn <codealicekarolyn@gmail.com>
2023-11-28 04:25:14 -0300

    tela de informações

---
+++ app/src/main/java/com/example/demoapp/ViewInformacoes.kt
@@ -0,0 +1,11 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewInformacoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_informacoes)
+    }
+}
